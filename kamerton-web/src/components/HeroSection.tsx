99ad6dbac1c9b42e961bfd6d2ec87ec21640bad1
Никита Сорокин <nikitasorokin@MacBook-Air-Nikita-2.local>
2025-11-14 21:14:24 +0300

    broke/hero-section

--- kamerton-web/src/components/HeroSection.tsx
+++ kamerton-web/src/components/HeroSection.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
 import "./HeroSection.css";
+import CityImg from '@img/city-img.svg'
 
 const HeroSection: React.FC = () => {
   return (
     <div className="hero-section">
             <div className="hero-content-left">
-                  <div className="hero-content-left-big"></div>
+                  <div className="hero-content-left-big">
+                      <h1>Самая эффективная реклама в Богородске </h1>
+                      <p>Ваш бизнес увидят даже те, кого не берут обычные таргетированные объявления. Максимальный охват новой аудитории.</p>
+                      <button>связаться</button>
+                  </div>
                   <div className="hero-content-left-small"></div>
                   <div className="hero-content-left-small"></div>
             </div>
             <div className="hero-content-right">
-                  <div className="LogoImage"></div>
+                  <img src={CityImg} alt="Banner" className="LogoImage"/>
             </div>
     </div>
   );
