9502e5d081df5a505b8e6731b37506855aa1ada9
Никита Сорокин <nikitasorokin@MacBook-Air-Nikita-2.local>
2025-11-16 21:10:46 +0300

    create/broken-hero-section

--- kamerton-web/src/components/HeroSection.tsx
+++ kamerton-web/src/components/HeroSection.tsx
@@ -1,24 +1,31 @@
 import * as React from "react";
 import "./HeroSection.css";
-import CityImg from '@img/city-img.svg'
+import HeroMiniImg from "@img/hero-mini.svg";
+import GraphImg from "@img/graph.svg";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="hero-section">
-            <div className="hero-content-left">
-                  <div className="hero-content-left-big">
+     <div className="hero-section">
+            <div className="hero-left-big">
+              <div className="hero-left-big-small">
                       <h1>Самая эффективная реклама в Богородске </h1>
-                      <p>Ваш бизнес увидят даже те, кого не берут обычные таргетированные объявления. Максимальный охват новой аудитории.</p>
-                      <button>связаться</button>
-                  </div>
-                  <div className="hero-content-left-small"></div>
-                  <div className="hero-content-left-small"></div>
+                         <p>Ваш бизнес увидят даже те, кого не берут обычные таргетированные объявления.
+                          Максимальный охват новой аудитории.</p>
+                          <button>связаться</button>
+              </div>
             </div>
-            <div className="hero-content-right">
-                  <img src={CityImg} alt="Banner" className="LogoImage"/>
+            <div className="hero-left-small-one">
+                  <div className="text-small-one">123</div>
+                  <div className="img-small-one">img</div>
             </div>
-    </div>
+            <div className="hero-left-small-two">
+                  <div className="text-small-one">123</div>
+                  <div className="img-small-one">img</div>
+            </div>
+            <div className="hero-right-big">
+            </div>
+     </div>
   );
-}
+};
 
 export default HeroSection;
