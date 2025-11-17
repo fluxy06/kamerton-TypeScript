3b11a496771d7c394842defebdff17a09eff6b0e
Никита Сорокин <nikitasorokin@MacBook-Air-Nikita-2.local>
2025-11-17 12:29:42 +0300

    info/text/create/universal

--- kamerton-web/src/components/info.tsx
+++ kamerton-web/src/components/info.tsx
@@ -9,16 +9,24 @@ const Info: React.FC = ({}) => {
       <p>Готовы начать работу? Свяжитесь с нами удобным для вас способом.</p>
       <div className="info-block">
             <div className="adres mini-info">
-
+                    <h1 className="text-mini-info-h1">📍</h1>
+                    <h2 className="text-mini-info-h2">Адрес</h2>
+                    <p className="text-mini-info-h3">ул. Ленина, 180,<br/>г. Богородск, 607600</p>
             </div>
             <div className="phone-number mini-info">
-
+                  <h1 className="text-mini-info-h1">📞</h1>
+                    <h2 className="text-mini-info-h2">Телефон</h2>
+                    <p className="text-mini-info-h3">+7 (987) 540-15-15<br/>+7 (123) 456-78-90</p>
             </div>
             <div className="email mini-info">
-
+                  <h1 className="text-mini-info-h1">✉️</h1>
+                    <h2 className="text-mini-info-h2">Адрес</h2>
+                    <p className="text-mini-info-h3">info@kamerton.ru<br/>kamertonbg@gmail.com</p>
             </div>
             <div className="grafic mini-info">
-
+                    <h1 className="text-mini-info-h1">🕒</h1>
+                    <h2 className="text-mini-info-h2">Режим работы</h2>
+                    <p className="text-mini-info-h3">Пн-Пт: 10:00 - 20:00<br/>Сб-Вс: 10:00 - 20:00</p>
             </div>
             <div className="map">
                 
